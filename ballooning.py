3fb084a0baa2ca7c013bd7308fd1e7b6454a0ca8
Sagar N.R <130398768+Sagarnr1997@users.noreply.github.com>
2026-08-15 00:14:01 +0530

    ballooning.py

--- ballooning.py
+++ ballooning.py
@@ -7,18 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1zkW5rXadFygYZtn5yeBXoLFtSAX9m9IL
 """
 
-pip install streamlit 
-
-pip install pymupdf 
-pip install opencv-python 
-
-pip install pillow 
-pip install numpy 
-pip install pandas
-
-pip install pytesseract
-
-pip install paddleocrb
 
 import fitz
 import re
