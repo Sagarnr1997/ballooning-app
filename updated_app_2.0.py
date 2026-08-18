87b35fd8348eda7f80459cbc5dc8f9ca68c1de7c
Sagar N.R <130398768+Sagarnr1997@users.noreply.github.com>
2026-08-18 08:43:32 +0530

    Update updated_app_2.0.py

--- updated_app_2.0.py
+++ updated_app_2.0.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyPeFiKfrJdP3cjPTAi6ZX3C",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
